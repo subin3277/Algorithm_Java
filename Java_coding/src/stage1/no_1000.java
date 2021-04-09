22bac7bdaf8429e6123e931b551f165be94262e4
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-10 02:48:38 +0900

    #1000

---
+++ Java_coding/src/stage1/no_1000.java
@@ -0,0 +1,16 @@
+package stage1;
+import java.util.Scanner;
+
+public class no_1000 {
+
+	public static void main(String[] args) {
+		Scanner num= new Scanner(System.in);
+		
+		int A = num.nextInt();
+		int B= num.nextInt();
+		
+		System.out.println(A+B);
+		
+	}
+
+}
