d5a838ee0be5d50e5f46ca0f26628c17f251abf5
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-10 02:49:10 +0900

    #10869

---
+++ Java_coding/src/stage1/no_10869.java
@@ -0,0 +1,19 @@
+package stage1;
+
+import java.util.Scanner;
+
+public class no_10869 {
+
+	public static void main(String[] args) {
+		Scanner num= new Scanner(System.in);
+		
+		int A = num.nextInt();
+		int B= num.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+}
