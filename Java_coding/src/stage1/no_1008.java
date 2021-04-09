39fdc07e4394f6fb86942a73d454d5c1098a2583
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-10 02:48:43 +0900

    #1008

---
+++ Java_coding/src/stage1/no_1008.java
@@ -0,0 +1,16 @@
+package stage1;
+import java.util.Scanner;
+
+public class no_1008 {
+
+	public static void main(String[] args) {
+		Scanner num= new Scanner(System.in);
+		
+		double A = num.nextDouble();
+		double B= num.nextDouble();
+		
+		System.out.println(A/B);
+
+	}
+
+}
