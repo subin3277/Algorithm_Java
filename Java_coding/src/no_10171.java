5c614d882f40071c8bd24edbd6b3b512ec0a8694
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-10 02:48:27 +0900

    no message

--- Java_coding/src/no_10171.java
+++
@@ -1,8 +0,0 @@
-
-public class no_10171 {
-
-	public static void main(String[] args) {
-		System.out.println("\\    /\\\r\n" + " )  ( ')\r\n" 
-	+"(  /  )\r\n" + " \\(__)|");
-	}
-}
--- Java_coding/src/no_2557.java
+++
@@ -1,9 +0,0 @@
-
-public class no_2557 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
-	}
-
-}
