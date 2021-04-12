11d1938c7d38229aff2d67a2f0a2474e31a72cb7
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-13 00:21:19 +0900

    #2739

---
+++ Java_coding/src/stage3/no_2739.java
@@ -0,0 +1,18 @@
+package stage3;
+
+import java.util.Scanner;
+
+public class no_2739 {
+
+	public static void main(String[] args) {
+		Scanner num= new Scanner(System.in);
+		
+		int A = num.nextInt();
+		
+		for(int i=1;i<10;i++) {
+			System.out.println(A+" * "+i+" = "+i*A);
+		}
+
+	}
+
+}
