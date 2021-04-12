af25b07fe26ba413a63934e572aab0fdcc6830f7
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-13 00:21:29 +0900

    #8393

---
+++ Java_coding/src/stage3/no_8393.java
@@ -0,0 +1,19 @@
+package stage3;
+
+import java.util.Scanner;
+
+public class no_8393 {
+
+	public static void main(String[] args) {
+		Scanner num= new Scanner(System.in);
+		
+		int A = num.nextInt();
+		int sum =0;
+		for(int i=0;i<A+1;i++) {
+			sum = sum+i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
