62b96f1157e3a0e589de207581d5f939f196d7cb
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-14 14:20:23 +0900

    #10951

---
+++ Java_coding/src/stage4/no_10951.java
@@ -0,0 +1,19 @@
+package stage4;
+
+import java.util.Scanner;
+
+public class no_10951 {
+
+	public static void main(String[] args) {
+		Scanner num= new Scanner(System.in);
+		
+		while (num.hasNextInt()) {
+			int A = num.nextInt();
+			int B = num.nextInt();
+			System.out.println(A+B);
+	
+		}
+
+	}
+
+}
