c3b39ee2a6e71d43a16601608ad3646ebc1e42c2
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-23 00:58:23 +0900

    #2675

---
+++ Java_coding/src/stage7/no_2675.java
@@ -0,0 +1,24 @@
+package stage7;
+
+import java.util.Scanner;
+
+public class no_2675 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+
+		for(int i=0;i<num;i++) {
+			int R = scanner. nextInt();
+			String S = scanner.next();
+			for (int j=0;j<S.length();j++) {
+				for (int k=0;k<R;k++) {
+					System.out.print(S.charAt(j));
+				}
+			}
+			System.out.println("");
+		}
+		scanner.close();
+	}
+
+}
