62f08463cc0254061f4e8e747fa8e4812aa83f06
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-22 22:14:41 +0900

    #1065

---
+++ Java_coding/src/stage6/no_1065.java
@@ -0,0 +1,42 @@
+package stage6;
+
+import java.util.Scanner;
+
+public class no_1065 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		int count = 0;
+		
+		for (int i=1;i<num+1;i++) {
+			if(count(i)==true)
+				count++;
+		}
+		System.out.println(count);
+	}
+	
+	private static boolean count(int A) {
+		if (A<100) {
+			return true;
+		} else if(A>=100 & A<1000) {
+			//a=123
+			int a = A/100;
+			int b = (A/10)%10;
+			int c = A%10;
+			if (a>=c) {
+				if ((a-b)==(b-c))
+					return true;
+				else
+					return false;
+			} else {
+				if((c-b)==(b-a))
+					return true;
+				else
+					return false;
+			}
+		}
+		return false;
+	}
+
+}
