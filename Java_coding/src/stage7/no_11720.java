11492a9823e48495bc95edeb720599e1ff87a433
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-22 22:59:52 +0900

    #11720

---
+++ Java_coding/src/stage7/no_11720.java
@@ -0,0 +1,21 @@
+package stage7;
+
+import java.util.Scanner;
+
+public class no_11720 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		
+		int sum=0;
+		String str = scanner.next();
+		for(int i=0;i<num;i++) {
+			int a = Character.getNumericValue(str.charAt(i));
+			sum+=a;
+		}
+		System.out.println(sum);
+		scanner.close();
+	}
+
+}
