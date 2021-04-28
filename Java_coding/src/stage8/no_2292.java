b72ab858debb867c9766d2c63aa57dc12c2df1e2
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-04-28 23:51:26 +0900

    #2292

---
+++ Java_coding/src/stage8/no_2292.java
@@ -0,0 +1,21 @@
+package stage8;
+
+import java.util.Scanner;
+
+public class no_2292 {
+
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		int A=0;
+		for(int i=0;i<N;i++) {
+			A+=i;
+			if(N<=1+6*A) {
+				System.out.println(i+1);
+				break;
+			}
+		}
+		scanner.close();
+	}
+}
