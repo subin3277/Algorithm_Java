8bbf15fd46e4c2496b2e97c4e6a3655b86af6091
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-05-02 02:54:25 +0900

    #1011

--- Java_coding/src/stage8/no_1011.java
+++ Java_coding/src/stage8/no_1011.java
@@ -1,9 +1,18 @@
 package stage8;
 
+import java.util.Scanner;
+
 public class no_1011 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int T = scanner.nextInt();
+		for(int i=0;i<T;i++) {
+			int x = scanner.nextInt();
+			int y = scanner.nextInt();
+			
+		}
 
 	}
 
