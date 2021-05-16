9c3c09382d244ae4eae6c5514f713c0f8bc60e1f
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-05-16 12:10:00 +0900

    #1929

--- Java_coding/src/stage9/no_1929.java
+++ Java_coding/src/stage9/no_1929.java
@@ -3,28 +3,34 @@ package stage9;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class no_1929 {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
+	
+	public static boolean[] prime;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int M = in.nextInt();
+		int N = in.nextInt();
 		
-		for(int i=M;i<N;i++) {
-			int count=0;
-			for(int j=2;j<i;j++) {
-				if(i%j==0) {
-					count=1;
-					break;
-				}
-			}
-			if(count==0)
-				System.out.println(i);
+		prime = new boolean[N + 1];
+		get_prime();
+				
+		for(int i = M; i <= N; i++) {
+			// false = 소수 
+			if(!prime[i]) System.out.println(i);
 		}
 	}
-
+		public static void get_prime() {
+			// true = 소수아님 , false = 소수 
+			prime[0] = prime[1] = true;
+			
+			for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+				if(prime[i]) continue;
+				for(int j = i * i; j < prime.length; j += i) {
+					prime[j] = true;
+				}
+			}
+		}
 }
