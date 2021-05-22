119c6a224b0c228fb6d4128e3704996d416c09e2
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-05-23 02:39:05 +0900

    #1085

--- Java_coding/src/stage9/no_1085.java
+++ Java_coding/src/stage9/no_1085.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class no_1085 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner in = new Scanner(System.in);
         
 		int x = in.nextInt();
