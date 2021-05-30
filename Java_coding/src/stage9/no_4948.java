3be636a08dcbd1b6eacf0811d26fa144eab54df2
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-05-30 23:24:12 +0900

    #4948

--- Java_coding/src/stage9/no_4948.java
+++ Java_coding/src/stage9/no_4948.java
@@ -3,40 +3,38 @@ package stage9;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class no_4948 {
+	
+	static boolean[] prime = new boolean[246913];
 
 	public static void main(String[] args) throws IOException{
-	
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
+       
+		getPrime();
+		
 		while (true) {
-			int A = Integer.parseInt(st.nextToken());
-			int count=0;
+			int A = Integer.parseInt(br.readLine());
 			if(A==0)
 				break;
-			else {
+			int count=0;
 				for(int i=A+1;i<(2*A)+1;i++) {
-					if (getPrime(i))
+					if (!prime[i])
 						count++;
 				}
 				System.out.println(count);
-			}
+			
 		}
 		
 	}
-	
-	public static boolean getPrime(int A) {
-		int i=2;
-		for(i=2;i<A+1;i++) {
-			if(A%i==0)
-				break;
+	public static void getPrime() {
+		prime[0]=prime[1]=true;
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
 		}
-		if (i==A)
-			return true;
-		else
-			return false;
 	}
 }
