7166361ac57726d61bc920bf839840597e13fe24
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-06-06 16:01:14 +0900

    #2231

--- Java_coding/src/stage11/no_2231.java
+++ Java_coding/src/stage11/no_2231.java
@@ -10,60 +10,21 @@ public class no_2231 {
 		int N = in.nextInt();
 		in.close();
 		
-		int sum=0;
-		
-		if(N<=18) {
-			if(N%2==0) {
-					sum=N/2;
-					System.out.println(sum);
-				}
-		} else if(N<118) {
-			int n = N-18;
-			for(int i=n;i<N;i++) {
-				sum = i+(i/10)+(i%10);
-				if(sum == N) {
-					System.out.println(i);
-					break;
-				}
+		int result=0;
+	
+		for(int i=0;i<N;i++) {
+			int number = i;
+			int sum=0;
+			
+			while (number !=0) {
+				sum +=number%10;
+				number/=10;
 			}
-		} else if(N<1027) {
-			int n = N-27;
-			for(int i=n;i<N;i++) {
-				sum = i+(i/100)+((i/10)%10)+(i%10);
-				if(sum == N) {
-					System.out.println(i);
-					break;
-				}
-			}
-		} else if(N<10036) {
-			int n = N-36;
-			for(int i=n;i<N;i++) {
-				sum = i+(i/1000)+((i/100)%10)+((i/10)%10)+(i%10);
-				if(sum == N) {
-					System.out.println(i);
-					break;
-				}
-			}
-		} else if(N<100045) {
-			int n = N-45;
-			for(int i=n;i<N;i++) {
-				sum = i+(i/10000)+((i/1000)%10)+((i/100)%10)+((i/10)%10)+(i%10);
-				if(sum == N) {
-					System.out.println(i);
-					break;
-				}
-			}
-		} else if(N<1000054) {
-			int n = N-54;
-			for(int i=n;i<N;i++) {
-				sum = i+(i/100000)+((i/10000)%10)+((i/1000)%10)+((i/100)%10)+((i/10)%10)+(i%10);
-				if(sum == N) {
-					System.out.println(i);
-					break;
-				}
+			if(sum+i==N) {
+				result=i;
+				break;
 			}
 		}
-		if(sum==0)
-			System.out.println("0");
-	}
+		System.out.println(result);
+	}		
 }
