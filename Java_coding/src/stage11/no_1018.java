4ff8973daa8234d00a8028401afa13c6e8cb7dba
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-06-13 00:18:25 +0900

    #1018

--- Java_coding/src/stage11/no_1018.java
+++ Java_coding/src/stage11/no_1018.java
@@ -1,9 +1,13 @@
 package stage11;
 
+import java.util.Scanner;
+
 public class no_1018 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
 
 	}
 
