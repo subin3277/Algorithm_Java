8ea5ec0fada81cccfaac02f83a0ffeee0e293b26
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-06-22 21:34:27 +0900

    #11651

---
+++ Java_coding/src/stage12/no_11651.java
@@ -0,0 +1,40 @@
+package stage12;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class no_11651 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		 
+		int N = in.nextInt();
+ 
+		int[][] array = new int[N][2];
+ 
+		for(int i = 0; i < N; i++) {
+			array[i][1] = in.nextInt();	// [i][0] : x ÁÂÇ¥ 
+			array[i][0] = in.nextInt();	// [i][1] : y ÁÂÇ¥
+		}
+		
+		Arrays.sort(array, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[0]==o2[0]) {
+					return o1[1]-o2[1];
+				} else {
+					return o1[0]-o2[0];
+				}
+				
+			}
+		});
+		
+		for(int i=0;i<N;i++) {
+			System.out.print(array[i][1]+" ");
+			System.out.println(array[i][0]);
+		}
+
+	}
+
+}
