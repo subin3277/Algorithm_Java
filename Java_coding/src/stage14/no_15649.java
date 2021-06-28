1a18f334141bc080d93a8204d46786791f0a4946
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-06-28 17:02:41 +0900

    #15649

---
+++ Java_coding/src/stage14/no_15649.java
@@ -0,0 +1,18 @@
+package stage14;
+
+import java.util.Scanner;
+
+public class no_15649 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		 
+		int N = in.nextInt();
+		int M = in.nextInt();
+		in.close();
+		
+		
+
+	}
+
+}
