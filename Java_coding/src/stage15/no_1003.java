9940f022940624b521fe45777818a7137e5a9ba8
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-06-30 21:32:59 +0900

    #1003

--- Java_coding/src/stage15/no_1003.java
+++ Java_coding/src/stage15/no_1003.java
@@ -3,31 +3,33 @@ package stage15;
 import java.util.Scanner;
 
 public class no_1003 {
-
-	static int count0, count1;
+	static Integer[][] dp = new Integer[41][2];
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
 		int T = in.nextInt();
-		for(int i=0;i<T;i++) {
+		
+
+		dp[0][0] = 1;	// N=0 일 때의 0 호출 횟수
+		dp[0][1] = 0;	// N=0 일 때의 1 호출 횟수
+		dp[1][0] = 0;	// N=1 일 때의 0 호출 횟수
+		dp[1][1] = 1;	// N=1 일 때의 1 호출 횟수
+		while(T-- > 0){
 			int N = in.nextInt();
-			count0 = 0;
-			count1 = 0;
 			fibonacci(N);
-			System.out.println(count0+" "+count1);
+			System.out.println(dp[N][0] + " " + dp[N][1]);
 		}
 		in.close();
 	}
 	
-	public static int fibonacci(int n) {
-		if(n==0) {
-			count0++;
-			return 0;
-		} else if(n==1) {
-			count1++;
-			return 1;
-		} else {
-			return fibonacci(n-1)+fibonacci(n-2);
+	static Integer[] fibonacci(int N) {
+		// N에 대한 0, 1의 호출 횟수가 없을 떄(탐색하지 않은 값일 때)
+		if(dp[N][0] == null || dp[N][1] == null) {
+		// 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
+			dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+			dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
 		}
+		// N에 대한 0과 1, 즉 [N][0]과 [N][1] 을 담고있는 [N]을 반환한다.
+		return dp[N];
 	}
 }
