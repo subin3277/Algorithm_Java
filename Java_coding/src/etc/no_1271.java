c1a932d15dca9306df5ebe8770eaaaa811cc1642
subin <subin3277@naver.com>
2022-03-10 11:24:36 +0900

    #1271

---
+++ Java_coding/src/etc/no_1271.java
@@ -0,0 +1,20 @@
+package etc;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class no_1271 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		BigInteger n = in.nextBigInteger();
+		BigInteger m = in.nextBigInteger();
+		
+		BigInteger res1 = n.divide(m);
+		BigInteger res2 = n.remainder(m);
+		
+		in.close();
+		System.out.println(res1);
+		System.out.println(res2);
+	}
+}
