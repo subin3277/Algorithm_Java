46a4f0a17d68301b6bea39d1e3504ca361904cb2
subin <subin3277@naver.com>
2022-03-10 11:25:57 +0900

    #1550

---
+++ Java_coding/src/etc/no_1550.java
@@ -0,0 +1,13 @@
+package etc;
+
+import java.util.Scanner;
+
+public class no_1550 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String str = in.next();
+		in.close();
+		System.out.println(Integer.parseInt(str,16));
+	}
+}
