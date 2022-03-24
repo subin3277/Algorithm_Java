2ee76edec010aae8b774564052cfa5838b5d67ad
subin <subin3277@naver.com>
2022-03-24 17:26:16 +0900

    #1676

---
+++ Java_coding/src/class3/no_1074.java
@@ -0,0 +1,20 @@
+package class3;
+
+import java.util.Scanner;
+
+public class no_1074 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int r = in.nextInt(); //행
+		int c = in.nextInt(); //열
+		double n = Math.pow(2, N)*Math.pow(2,N);
+		
+		while(r<Math.pow(2, N)) {
+			
+		}
+	}
+
+}
