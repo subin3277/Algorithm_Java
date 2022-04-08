c7f5d29ef30f5873307870d0c1ae612aa70170f4
subin <subin3277@naver.com>
2022-04-08 17:13:44 +0900

    #2108

--- Java_coding/src/class2/no_1920.java
+++ Java_coding/src/class2/no_1920.java
@@ -1,6 +1,6 @@
 package class2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class no_1920 {
@@ -9,21 +9,39 @@ public class no_1920 {
 		Scanner in = new Scanner(System.in);
 		
 		int N = in.nextInt();
-		ArrayList<Integer> orilist = new ArrayList<>();
-		for(int i=0;i<N;i++) {
-			int num = in.nextInt();
-			orilist.add(num);
+		int[] list = new int[N];
+		for(int i = 0;i<N;i++) {
+			list[i] = in.nextInt();
 		}
+		Arrays.sort(list);
 		
 		int M = in.nextInt();
 		for(int j=0;j<M;j++) {
 			int num =in.nextInt();
-			if(orilist.contains(num))
+			if(find(num, list))
 				System.out.println("1");
 			else
 				System.out.println("0");
 		}
 		in.close();		
+	}
+	
+	static boolean find(int num, int[] list) {
+		int min = 0;
+		int max = list.length-1;
+		boolean state = false;
+		while (min<=max) {
+			int mid = (min+max)/2;
+			if(num==list[mid]) {
+				state = true;
+				break;
+			}
+			else if (num<list[mid])
+				max = mid-1;
+			else
+				min = mid+1;
+		}
+		return state;
 	}
 
 }
