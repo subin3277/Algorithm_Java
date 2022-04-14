19dd444acf0024cb7021494244b961bc98bae025
subin <subin3277@naver.com>
2022-04-14 10:03:59 +0900

    #2164

---
+++ Java_coding/src/class2/no_2164.java
@@ -0,0 +1,25 @@
+package class2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class no_2164 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int N = in.nextInt();
+		
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i=1;i<N+1;i++)
+			queue.add(i);
+		while(queue.size()!=1) {
+			queue.remove();
+			queue.add(queue.poll());
+		}
+		System.out.println(queue.peek());
+
+	}
+
+}
