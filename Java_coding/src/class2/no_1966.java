5260f1a58a3156149e19db713cdfb5331bf45858
subin Kim <subin3277@naver.com>
2022-04-23 23:32:08 +0900

    #1966

--- Java_coding/src/class2/no_1966.java
+++ Java_coding/src/class2/no_1966.java
@@ -1,34 +1,50 @@
 package class2;
 
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class no_1966 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
 		int K = in.nextInt();
 		
-		Queue<int[]> queue = new LinkedList<>();
-		for (int i=0;i<K;i++) {
+		while(K-- > 0) {
 			int N = in.nextInt();
 			int M = in.nextInt();
-			int index = 0;
-			int[] first;
-			int count = 0;
 			
-			for(int j=0;j<N;j++) {
-				int tmp = in.nextInt();
-				int[] tmplist = new int[2];
-				tmplist[0] = j;
-				tmplist[1] = tmp;
-				queue.add(tmplist);
+			LinkedList<int[]> queue = new LinkedList<>();
+			for(int j = 0;j<N;j++) {
+				queue.offer(new int[] {j,in.nextInt()});
 			}
 			
+			int count = 0;
 			
+			while (!queue.isEmpty()) {
+				int[] front = queue.poll();
+				boolean isMax = true;
+				
+				for(int i=0;i<queue.size();i++) {
+					if(front[1]<queue.get(i)[1]) {
+						queue.offer(front);
+						for(int j =0;j<i;j++) {
+							queue.offer(queue.poll());
+						}
+						isMax = false;
+						break;
+					}
+				}
+				if(isMax == false)
+					continue;
+				count++;
+				if(front[0]==M)
+					break;
+			}
+			sb.append(count).append('\n');
 		}
 		in.close();
+		System.out.println(sb);
 	}
 
 }
