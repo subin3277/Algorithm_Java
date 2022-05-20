98d14dbe5dafd462c60b9077fb74009b590ce2ef
subin <subin3277@naver.com>
2022-05-20 13:23:40 +0900

    #2525

---
+++ Java_coding/src/stage2/no_2525.java
@@ -0,0 +1,25 @@
+package stage2;
+
+import java.util.Scanner;
+
+public class no_2525 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		int C = in.nextInt();
+		
+		if(B+C>=60) {
+			int num = (B+C)/60;
+			A+=num;
+			B=B+C-(num*60);
+		} else
+			B=B+C;
+		if(A==24)
+			A=0;
+		System.out.println(A+" "+B);
+
+	}
+
+}
