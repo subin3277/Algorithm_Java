055350873682e63f5de224bf99b0f12d86440f2f
subin <subin3277@naver.com>
2022-05-20 10:12:58 +0900

    #18108

---
+++ Java_coding/src/stage1/no_18108.java
@@ -0,0 +1,14 @@
+package stage1;
+
+import java.util.Scanner;
+
+public class no_18108 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int year = in.nextInt();
+		System.out.println(year-543);
+		in.close();
+	}
+
+}
