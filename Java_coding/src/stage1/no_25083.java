1c984db5d4ce5530e4e7b58e6ab674d06b30d17e
subin <subin3277@naver.com>
2022-05-20 10:07:56 +0900

    #25083

---
+++ Java_coding/src/stage1/no_25083.java
@@ -0,0 +1,15 @@
+package stage1;
+
+public class no_25083 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+	}
+
+}
