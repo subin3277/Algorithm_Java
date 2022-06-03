a3f20523e53ed88f56ebc66c674c927e3c413cc4
subin <subin3277@naver.com>
2022-06-03 16:35:09 +0900

    #18111

--- Java_coding/src/class2/no_10989.java
+++ Java_coding/src/class2/no_10989.java
@@ -3,7 +3,6 @@ package class2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class no_10989 {
 
---
+++ Java_coding/src/class2/no_18111.java
@@ -0,0 +1,64 @@
+package class2;
+
+import java.io.*;
+import java.util.*;
+
+public class no_18111 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		int max = -1;
+		int min = 257;
+		
+		int[][] list = new int[N][M];
+		
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				int a = Integer.parseInt(st.nextToken());
+				list[i][j] = a;
+				max = Math.max(a, max);
+				min = Math.min(a, min);
+			}
+		}
+		
+		
+		int minsec = Integer.MAX_VALUE;
+		int dis = min;
+		
+		for (int k=min;k<max+1;k++) {
+			int sec = 0;
+			int block = B;
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<M;j++) {
+					int tmp = list[i][j] - k; 
+					if(tmp > 0) {
+						sec+=2*tmp;
+						block += tmp;
+					} else {
+						sec-=tmp;
+						block+=tmp;
+					}
+				}
+			}
+			if(minsec>=sec&&block>=0) {
+				if(sec == minsec)
+					dis = dis>k ? dis:k;
+				else {
+					minsec = sec;
+					dis = k;	
+				}
+			
+				
+			}
+		}
+		
+		System.out.println(minsec +" "+dis);
+	}
+
+}
